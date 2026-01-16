806443f0c4c8c388bfab0b2075a74053f7f9af56
anshika051103 <danshika1105@gmail.com>
2026-01-16 23:13:27 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,11 @@
+# Task 2: List Slicing
+
+numbers = list(range(1, 11))
+
+print("Original list:", numbers)
+
+first_five = numbers[:5]
+print("Extracted first five elements:", first_five)
+
+reversed_list = first_five[::-1]
+print("Reversed extracted elements:", reversed_list)
